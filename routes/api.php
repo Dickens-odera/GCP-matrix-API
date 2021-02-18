7e90506fc088a7432b6c50565a6b74c137934a7d
Dickens-odera <dickensodera9@gmail.com>
2021-02-18 16:00:43 +0300

    Removed unnecessary comments and users api route

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LocationController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::group(['prefix' => '/champions/v2/maps', 'as'=> 'champions.maps.'], function()
 {
     Route::post('/distance',[LocationController::class,'getDistance'])->name('distance');
